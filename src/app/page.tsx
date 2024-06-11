f51d25330d44723f2657959f9ca9e1ad1267aeb3
Alex Felix <alex.fellix@outlook.com>
2024-06-10 21:08:06 -0300

    website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,171 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <header className={styles.headerContainer}>
+        <div className={styles.headerDiv}>
+          <h1 className={styles.headerH1}>Naufaldi</h1>
+          <nav className={styles.nav}>
+            <ul className={styles.headerUl}>
+              <li className={styles.headerLi}>Home</li>
+              <li className={styles.headerLi}>Mentorship</li>
+              <li className={styles.headerLi}>Portfolio</li>
+              <li className={styles.headerLi}>Snippet</li>
+              <li className={styles.headerLi}>Blog</li>
+            </ul>
+          </nav>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div className={styles.headerHr}>
+          <hr className={styles.headerHr}/>
+        </div>
+      </header>
+      <main className={styles.main}>
+        <section className={styles.mainSection}>
+          <h1 className={styles.sectionH1}>
+            Hi Im Naufaldi, a special human with some ability to love learning
+            and working on teamwork.
+          </h1>
+          <div className={styles.about}>
+            <div className={styles.image}>
+              <Image
+                src="/perfil.png"
+                width={160}
+                height={160}
+                alt="foto1"
+              />
+            </div>
+            <div className={styles.biography}>
+              <div>
+                <h3 className={styles.sectionH3}>Biography</h3>
+                <p className={styles.sectionP}>
+                  Getting Buff +1 for learning, Buff +2 for documentation and 
+                  more buff on managing team. Exicited on React, UX Research 
+                  and Agile.
+                </p>
+              </div>
+              <div className={styles.contact}>
+                <h3 className={styles.sectionH3}>Lets connect</h3>
+                <div className={styles.contacListItem}>
+                  <div className={styles.contactIcon}> 
+                    <Image
+                      src="/facebook.png"
+                      width={30}
+                      height={30}
+                      alt="foto2"
+                    />
+                  </div>
+                  <div className={styles.contactIcon}>
+                    <Image
+                      src="/twiter.png"
+                      width={30}
+                      height={30}
+                      alt="foto3"
+                    />
+                  </div>
+                  <div className={styles.contactIcon}>
+                    <Image
+                      src="/instagram.png"
+                      width={30}
+                      height={30}
+                      alt="foto4"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h1>Oque eu faço</h1>
+            <p>
+              programo sites para empresas
+              e para meus beneficios.
+            </p>
+          </div>         
+          <div>
+            <h1>Desenvolvimento web</h1>
+            <p>
+              Você receberá um personalizado
+              planeje sua jornada de fitness,
+              e muito apoio.
+            </p>
+          </div>
+          <div>
+            <h1>Pesquisa de experiência do usuário.</h1>
+            <p>
+              Você receberá um personalizado
+              planeje sua jornada de fitness,
+              e muito apoio.
+            </p>
+          </div>
+        </section>
+        <section>
+          <h1 className={styles.featuredH1}>Featured Project</h1>
+          
+          <div className={styles.sectionContainer}>
+            <div className={styles.sectionBox}></div>
+            <div className={styles.sectionBoxItems}>
+              <h3 className={styles.sectionBoxItemOne}>Web Development</h3>
+              <h2 className={styles.sectionBoxItemTwo}>Border Landingpage</h2>
+              <p className={styles.sectionBoxItemThree}>
+                Serrow restructured and designed core pages, creating
+                interactive elements that put users in control and allowed them
+                to discover the information needed to make a decision
+              </p>
+            </div>
+          </div>
+          <div className={styles.sectionContainerTwo}>
+            <div className={styles.sectionBoxTwo}></div>
+            <div className={styles.sectionBoxItems}>
+              <h3 className={styles.sectionBoxItemOne}>Desenvolvimento web</h3>
+              <h2 className={styles.sectionBoxItemTwo}>Educação Kerja Mantul Gerenciamento</h2>
+              <p className={styles.sectionBoxItemThree}>
+                Serrow restructured and designed core pages, creating
+                interactive elements that put users in control and allowed them
+                to discover the information needed to make a decision.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <h1 className={styles.containerH1}>Code Snippet</h1>
+          <div className={styles.container}>
+            <div className={styles.containerBox}>
+              <h3>Inicioador nextjs</h3>
+              <p>Um projeto nextjs para um mero mortal.</p>
+              <h5>8 estrelas</h5>
+            </div>
+            <div className={styles.containerBoxTwo}>
+              <h3>Kit inicial frontal</h3>
+              <p>
+                um projeto extremamente simples para html 5 bolierplate.
+                configuração incluída para minify css, html minify,
+                configuração de SEO.
+              </p>
+              <h5>12 estrela</h5>
+            </div>
+          </div>
+          <div className={styles.containerTwo}>
+            <div className={styles.containerBoxThree}>
+              <h3>Fragmento de código</h3>
+              <p>Um projeto nextjs para um mero mortal.</p>
+              <h5>8 estrelas</h5>
+            </div>
+            <div className={styles.containerBoxFour}>
+              <h3>Kit inicial frontal</h3>
+              <p>
+                um projeto extremamente simples para html 5 bolierplate.
+                configuração incluída para minify css, html minify,
+                configuração de SEO.
+              </p>
+              <h5>12 estrelas</h5>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
   );
 }
---
+++ src/utils/global.fonts.ts
@@ -0,0 +1,220 @@
+import localFont from 'next/font/local';
+
+const poppinsBlack = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Poppins/Poppins-Black.ttf',
+      weight: '400',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-poppinsBlack',
+  preload: true
+});
+
+const poppinsBlackItalic = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Poppins/poppins-BlackItalic.ttf',
+      weight: '400',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-poppinsBlackItalic',
+  preload: true
+});
+
+const poppinsBold = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Poppins/poppins-Bold.ttf',
+      weight: '400',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-poppinsBold',
+  preload: true
+});
+
+const poppinsBoldItalic = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Poppins/poppins-BoldItalic.ttf',
+      weight: '400',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-poppinsBoldItalic',
+  preload: true
+});
+
+const poppinsExtraBold = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Poppins/poppins-ExtraBold.ttf',
+      weight: '400',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-poppinsExtraBold',
+  preload: true
+});
+
+const poppinsExtraBoldItalic = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Poppins/poppins-ExtraBoldItalic.ttf',
+      weight: '400',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-poppinsExtraBoldItalic',
+  preload: true
+});
+
+const poppinsExtraLight = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Poppins/poppins-ExtraLight.ttf',
+      weight: '400',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-poppinsExtraLight',
+  preload: true
+});
+
+const poppinsExtraLightItalic = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Poppins/poppins-ExtraLightItalic.ttf',
+      weight: '400',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-poppinsExtraLightItalic',
+  preload: true
+});
+
+const poppinsItalic = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Poppins/poppins-Italic.ttf',
+      weight: '400',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-poppinsItalic',
+  preload: true
+});
+
+const poppinsLight = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Poppins/poppins-Light.ttf',
+      weight: '400',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-poppinsLight',
+  preload: true
+});
+
+const poppinsLightItalic = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Poppins/poppins-LightItalic.ttf',
+      weight: '400',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-poppinsLightItalic',
+  preload: true
+});
+
+const poppinsMedium = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Poppins/poppins-Medium.ttf',
+      weight: '400',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-poppinsMedium',
+  preload: true
+});
+
+const poppinsMediumItalic = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Poppins/poppins-MediumItalic.ttf',
+      weight: '400',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-poppinsMediumItalic',
+  preload: true
+});
+
+const poppinsRegular = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Poppins/Poppins-Regular.ttf',
+      weight: '400',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-poppinsRegular',
+  preload: true
+});
+
+const poppinsSemiBold = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Poppins/poppins-SemiBold.ttf',
+      weight: '400',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-poppinsSemiBold',
+  preload: true
+}); 
+
+const poppinsSemiBoldItalic = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Poppins/poppins-SemiBoldItalic.ttf',
+      weight: '400',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-poppinsSemiBoldItalic',
+  preload: true
+});
+
+const poppinsThin = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Poppins/poppins-Thin.ttf',
+      weight: '400',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-poppinsThin',
+  preload: true
+});
+
+const poppinsThinItalic = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Poppins/poppins-ThinItalic.ttf',
+      weight: '400',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-poppinsThinItalic',
+  preload: true
+});
+
+
+export {poppinsBlack, poppinsBlackItalic, poppinsBold, poppinsBoldItalic, poppinsExtraBold, poppinsExtraBoldItalic, poppinsExtraLight, poppinsLightItalic, poppinsItalic, poppinsLight, poppinsExtraLightItalic, poppinsMedium, poppinsMediumItalic, poppinsRegular, poppinsSemiBold, poppinsSemiBoldItalic, poppinsThin, poppinsThinItalic};
